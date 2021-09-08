4ab7cfe48134b88c9195ade4d01e19a6c4893892
Prakash Manglanathan <pmnathan@gmail.com>
2021-09-08 11:14:12 -0400

    Update main.tf

--- _examples/aks/kubernetes-config/main.tf
+++ _examples/aks/kubernetes-config/main.tf
@@ -42,6 +42,12 @@ resource "kubernetes_namespace" "test5" {
   }
 }
 
+resource "kubernetes_namespace" "test6" {
+  metadata {
+    name = "test6"
+  }
+}
+
 resource "kubernetes_deployment" "test" {
   metadata {
     name = "test"
